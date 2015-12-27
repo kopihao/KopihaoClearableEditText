c69d4920f1871a87786d7e48dc73358fe8d0c7b8
kopihao <kopihao@gmail.com>
2015-12-28 03:20:26 +0800

    Provide show case. Change 4th button to demo: Implementation of CET within AlerDialog.

--- src/com/kopihao/cet/demo/Activity_Main.java
+++ src/com/kopihao/cet/demo/Activity_Main.java
@@ -3,7 +3,11 @@ package com.kopihao.cet.demo;
 import com.view.plugin.cet.ClearableEditText;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -14,8 +18,7 @@ import android.widget.Toast;
  * This UI component is absolutely inherited from native EditText.<br>
  * This UI component extend capabilities and source code from :<br>
  * [KopihaoActiconEditText](https://github.com/kopihao/KopihaoActiconEditText)
-// *
- * <u>Why use this</u><br>
+ * // * <u>Why use this</u><br>
  * Custom UI component as a View rather than a View Group<br>
  * Inherit from EditText<br>
  * Native EditText attributes are usable directly in xml<br>
@@ -54,6 +57,44 @@ public class Activity_Main extends Activity {
 		ivIcon3.setOnClickListener(oic_ivIcon);
 		ivIcon4.setOnClickListener(oic_ivIcon);
 
+		String text = "1st, 2nd, 3rd button will change CET icon on main layout.\n";
+		text += "4th Button will pop alert dialog which contain CET.";
+		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+
+	}
+
+	private AlertDialog CETDialog(int resid) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		final ClearableEditText input;
+		builder.setTitle("Alert Dialog Showcase");
+
+		if (resid == 0) {
+			builder.setMessage("Type something:");
+			input = new ClearableEditText(this);
+			builder.setView(input);
+
+		} else {
+			LayoutInflater inflater = getLayoutInflater();
+			View dgView = inflater.inflate(resid, null);
+			builder.setView(dgView);
+			input = (ClearableEditText) dgView.findViewById(R.id.cetDgInput);
+		}
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String value = input.getText().toString();
+				Log.wtf("Jasper", "ClearableEditText,input::" + value);
+				return;
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				return;
+			}
+		});
+		return builder.create();
 	}
 
 	OnClickListener oic_ivIcon = new OnClickListener() {
@@ -75,8 +116,11 @@ public class Activity_Main extends Activity {
 
 				break;
 			case R.id.ivIcon4:
-				cetInput.setClearIcon(android.R.drawable.ic_input_get);
-				break;
+				// cetInput.setClearIcon(android.R.drawable.ic_input_get);
+				// break;
+				// CETDialog(0).show();
+				CETDialog(R.layout.dg_cet).show();
+				return;
 
 			default:
 				return;
